c052857cc287f07d1eb53639f844c82a1168cf7c
Niels Nübel <n.nuebel@nn-medienagentur.de>
2015-09-01 08:04:21 +0200

    since date

--- js/jennyundniels.js
+++ js/jennyundniels.js
@@ -23630,7 +23630,7 @@ $(document).ready(function(){
 		console.log(untilDate);
 
 		$(".countdown").countdown({
-			until: untilDate,
+			since: untilDate,
 			format: 'dHMS',
 			padZeroes: true
 		});
